6f3df23f247426e768a018a41caf4b890987914c
abubjazov <abubjazov@mail.ru>
2022-02-15 20:17:46 +0300

    AppHeader.spec: refactor

--- src/components/AppHeader/AppHeader.spec.js
+++ src/components/AppHeader/AppHeader.spec.js
@@ -2,16 +2,17 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import { AppHeader } from './AppHeader'
 
+const setUp = (props) => shallow(<AppHeader {...props}/>)
+
 describe ('should render AppHeader component', () => {
 	it ('should contain .app__header wrapper', () => {
-		const component = shallow(<AppHeader />)
+		const component = setUp()
 		const wrapper = component.find('.app__header')
 		expect(wrapper.length).toBe(1)
-		// console.log(component.debug())
 	})
 	
 	it ('should contain NavLink elements', () => {
-		const component = shallow(<AppHeader />)
+		const component = setUp()
 		const wrapper = component.find('NavLink')
 		expect(wrapper.length).toBe(2)
 	})  
