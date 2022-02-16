faf58b5f1acc0dcc481fd3191b06a6883853b3e6
abubjazov <abubjazov@mail.ru>
2022-02-16 16:26:39 +0300

    components/Posts/: .spec > change: 'shallow' to 'render'

--- src/components/Posts/Posts.spec.js
+++ src/components/Posts/Posts.spec.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { render, shallow } from 'enzyme'
 
 import Posts from './Posts'
 
 describe('Posts component', () => {
 	it('should render Post component', () => {
-		const component = shallow(<Posts />)
+		const component = render(<Posts />)
 		expect(component).toMatchSnapshot()
 	})
 })
