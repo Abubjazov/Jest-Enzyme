cb2b97cb90b66f070d2fe2f1b0fcc0255940e0b6
abubjazov <abubjazov@mail.ru>
2022-02-17 18:49:13 +0300

    components/Post: refactor all

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,22 +1,77 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-import { Post } from '..'
-import { NEWS } from '../constants/constants'
+import { Post, Title, Select, Input } from ".."
 
-export const Posts = () => (
-  <ul className='newsList'>
-    {NEWS.map(
-      ({ author, created_at, num_comments, objectID, title, points, url }) => (
-        <Post
-          key={objectID}
-          author={author}
-          created_at={created_at}
-          num_comments={num_comments}
-          title={title}
-          points={points}
-          url={url}
+import { NEWS, HITS } from '../constants/constants'
+
+export class Posts extends Component {
+  state = {
+    searchQuery: '',
+    hitsPerPage: 20,
+    page: 0
+  }
+
+  handleInputChange = ({ target: { value } }) => {
+    this.setState({
+      searchQuery: value
+    })
+  }
+
+  handleHitsChange = ({ target: { value } }) => {
+    this.setState({
+      hitsPerPage: +value,
+      page: 0
+    })
+  }
+
+  getSearch = ({ key }) => {
+    if (key === 'Enter') {
+      this.setState({
+        page: 0
+      })
+    }
+  }
+
+  render() {
+    const { searchQuery, hitsPerPage } = this.state
+
+    return (
+      <div className='wrapper'>
+        <Title title='Hacker News' />
+        <Select
+          options={HITS}
+          handleChange={this.handleHitsChange}
+          value={hitsPerPage}
         />
-      )
-    )}
-  </ul>
-)
+        <Input
+          onKeyPress={this.getSearch}
+          onChange={this.handleInputChange}
+          value={searchQuery}
+        />
+        <ul className='newsList'>
+          {NEWS.map(
+            ({
+              author,
+              created_at,
+              num_comments,
+              objectID,
+              title,
+              points,
+              url
+            }) => (
+              <Post
+                key={objectID}
+                author={author}
+                created_at={created_at}
+                num_comments={num_comments}
+                title={title}
+                points={points}
+                url={url}
+              />
+            )
+          )}
+        </ul>
+      </div>
+    )
+  }
+}
