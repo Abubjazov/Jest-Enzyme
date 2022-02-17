9fdc3a8742f279ca17f6b57649e2bac2b124eb47
abubjazov <abubjazov@mail.ru>
2022-02-17 20:40:18 +0300

    components/: +Info

---
+++ src/components/Info/Info.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+
+export class Info extends Component {
+  state = {
+    value: 'Test value',
+    width: 0
+  }
+
+  componentDidMount() {
+    this.handleChangeTitle()
+    window.addEventListener('resize', this.handleWidth)
+  }
+
+  componentDidUpdate() {
+    this.handleChangeTitle()
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleWidth)
+  }
+
+  handleChangeTitle = () => {
+    document.title = this.state.value
+  }
+
+  handleWidth = () => {
+    this.setState({
+      width: window.innerWidth
+    })
+  }
+
+  render() {
+    return <h1>{this.state.width}</h1>
+  }
+}
