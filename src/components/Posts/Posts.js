df2b75ae7bf5f346548c001298555f71267cdd51
abubjazov <abubjazov@mail.ru>
2022-02-17 21:50:34 +0300

    Posts: functionality+

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,39 +1,93 @@
 import React, { Component } from 'react'
 
-import { Post, Title, Select, Input } from ".."
+import {
+  Post,
+  Title,
+  Select,
+  Input,
+  Pagination
+} from '..'
 
-import { NEWS, HITS } from '../constants/postConstants'
+import {
+  NEWS,
+  HITS,
+  BASE_PATH,
+  SEARCH_PATH,
+  SEARCH_PARAM,
+  PAGE_HITS,
+  PAGE_PARAM
+} from '../constants/postConstants'
 
 export class Posts extends Component {
   state = {
     searchQuery: '',
+    result: {},
     hitsPerPage: 20,
     page: 0
   }
 
+  componentDidMount() {
+    const { searchQuery, hitsPerPage, page } = this.state
+
+    this.fetchData(searchQuery, hitsPerPage, page)
+  }
+
+  fetchData = (searchQuery, hitsPerPage, page) => {
+    fetch(`${BASE_PATH}${SEARCH_PATH}?${SEARCH_PARAM}${searchQuery}&${PAGE_HITS}${hitsPerPage}&${PAGE_PARAM}${page}`)
+      .then((res) => res.json())
+      .then((result) => this.setNews(result))
+  }
+
   handleInputChange = ({ target: { value } }) => {
-    this.setState({
-      searchQuery: value
-    })
+    this.setState({ searchQuery: value })
   }
 
   handleHitsChange = ({ target: { value } }) => {
-    this.setState({
-      hitsPerPage: +value,
-      page: 0
-    })
+    const { searchQuery } = this.state
+
+    this.setState(
+      {
+        hitsPerPage: +value,
+        page: 0
+      },
+      () => this.fetchData(searchQuery, this.state.hitsPerPage, 0)
+    )
   }
 
   getSearch = ({ key }) => {
     if (key === 'Enter') {
-      this.setState({
-        page: 0
-      })
+      const { searchQuery, hitsPerPage } = this.state
+
+      this.setState({ page: 0 })
+      this.fetchData(searchQuery, hitsPerPage, 0)
     }
   }
 
-  render() {
+  setNews = (result) => {
+    this.setState({ result })
+  }
+
+  handlePageChange = ({ target }) => {
+    const btnType = target.getAttribute('data-name')
+    let { page } = this.state
+
+    if (!isNaN(btnType)) {
+      this.updatePage(+btnType)
+    } else {
+      if (btnType === 'next') this.updatePage(page + 1)
+      if (btnType === 'prev') this.updatePage(page - 1)
+    }
+  }
+
+  updatePage = (page) => {
     const { searchQuery, hitsPerPage } = this.state
+
+    this.setState({ page }, () => this.fetchData(searchQuery, hitsPerPage, page))
+  }
+
+  render() {
+    const { searchQuery, result, hitsPerPage } = this.state
+    const { hits = [], page, nbPages } = result
 
     return (
       <div className='wrapper'>
@@ -43,13 +97,20 @@ export class Posts extends Component {
           handleChange={this.handleHitsChange}
           value={hitsPerPage}
         />
+        {hits.length > 0 && (
+          <Pagination
+            onClick={this.handlePageChange}
+            page={page}
+            lastPage={nbPages}
+          />
+        )}
         <Input
           onKeyPress={this.getSearch}
           onChange={this.handleInputChange}
           value={searchQuery}
         />
         <ul className='newsList'>
-          {NEWS.map(
+          {hits.map(
             ({
               author,
               created_at,
