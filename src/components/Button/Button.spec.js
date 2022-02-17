48afbd1b87150570f5dc3b3a9177787497b435a0
abubjazov <abubjazov@mail.ru>
2022-02-17 19:56:08 +0300

    Button: +Button.spec.js

---
+++ src/components/Button/Button.spec.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from '..'
+
+describe('Button component', () => {
+  it('should call onClick method', () => {
+    const mockCallBack = jest.fn()
+    const component = shallow(<Button onClick={mockCallBack} />)
+
+    expect(mockCallBack.mock.calls.length).toBe(0)
+    component.find('.btn').simulate('click')
+    expect(mockCallBack.mock.calls.length).toBe(1)
+  })
+})
