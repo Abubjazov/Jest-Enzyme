eff110d6e3a7843bfcdd955f33468ad0459bab76
abubjazov <abubjazov@mail.ru>
2022-02-17 23:54:14 +0300

    components/: +Context

---
+++ src/components/Context/Context.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+export const AuthContext = React.createContext()
+
+export const AuthProvider = ({ children }) => {
+  const [isLoggedIn, toggleLoginStatus] = useState(false)
+  const toggleLogin = () => toggleLoginStatus(!isLoggedIn)
+
+  return (
+    <AuthContext.Provider value={{ toggleLogin, isLoggedIn }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
